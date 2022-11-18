1d378a8b8496d8b2a5048ce062f33abad8af30c0
Blake Bybee <Blakebybeee@gmail.com>
2022-11-17 22:59:16 -0700

    final commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ List<string>time;
 time = new List<string>();
 List <string> finished;
 finished = new List<string>();
+string[] questions = new string[10];
+        questions[0] = "Hi";
+        questions[1] = "How are you ";
+        questions[2] = "Are you doing well";
+        questions[3] = "hey";
+        questions[4] = "yo";
+        questions[5] = "whats up";
+        questions[6] = "what you doin";
+        questions[7] = "you look nice today";
+        questions[8] = "greetings";
+        questions[9] = "what a do";
+        Random rnd = new Random();
+        Console.WriteLine(questions[rnd.Next(0,9)]);
 Console.WriteLine("what is the first item you want to do");
 string Firsttodo = Console.ReadLine();
 todolist.Add(Firsttodo);
@@ -38,14 +51,20 @@ for(int x = 0; x < todolist.Count; x++){
   }
   else {
     Console.WriteLine("sorry I don't understand. Please type yes or no");
-
+continue;
   }
 }
 for(int x =0; x <todolist.Count; x++){
   Console.WriteLine($"{todolist[x]}:");
   Console.WriteLine($"{time[x]}");
   Console.WriteLine(finished[x]);
+  File.WriteAllText("stats.csv", todolist[x]);
+File.WriteAllText("stats.cvs", time[x]);
+File.WriteAllText("stats.csv", finished[x]);
+  File.ReadAllText("stats.csv");
 }
+
+
 Console.ReadKey();
 
 void additem (){
@@ -56,12 +75,12 @@ void additem (){
   addtime = Console.ReadLine();
   time.Add(addtime);
 }
-if (File.Exists("Message.txt")){
-string previous = File.ReadAllText("Message.txt");
-Console.WriteLine("Last time, you said this : "+previous);
+if (File.Exists("stats.csv")){
+  
+ File.ReadAllText("stats.csv" );
+Console.WriteLine("total amount of to do items : "+todolist.Count);
 }
 Console.Write("what do you want me to tell you next time");
-string message = Console.ReadLine();
-File.WriteAllText("Message.txt", message);
+string? Message = Console.ReadLine();
 
 
